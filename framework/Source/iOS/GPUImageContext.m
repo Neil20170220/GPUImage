86fa63fa2eedf24b68ec6c73c8438c6b465d99bb
Neil <shengzhi.chen@ubnt.com>
2017-12-21 15:40:38 +0800

    remove log

--- framework/Source/iOS/GPUImageContext.m
+++ framework/Source/iOS/GPUImageContext.m
@@ -56,7 +56,6 @@ static void *openGLESContextQueueKey;
         CFRelease(_coreVideoTextureCache);
         _coreVideoTextureCache = NULL;
     }
-    NSLog(@" %@ dealloc", self.class);
 }
 
 + (void *)contextKey {
